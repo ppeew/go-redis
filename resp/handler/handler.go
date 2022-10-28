666517c418b2fcf4f7bbfdbabc88432f3bba2a0b
hyj <2237853288@qq.com>
2022-10-28 12:15:20 +0800

    gogo

--- resp/handler/handler.go
+++ resp/handler/handler.go
@@ -1,16 +1,13 @@
 package handler
 
 import (
-	"bufio"
 	"context"
 	databaseface "go-redis/interface/database"
 	"go-redis/lib/logger"
 	"go-redis/lib/sync/atomic"
-	"go-redis/lib/sync/wait"
-	"io"
+	"go-redis/resp/connection"
 	"net"
 	"sync"
-	"time"
 )
 
 type RespHandler struct {
@@ -19,56 +16,18 @@ type RespHandler struct {
 	closing    atomic.Boolean
 }
 
-func MakeEchoHandler() *EchoHandler {
-	return &EchoHandler{}
+func (handler *RespHandler) Handler(ctx context.Context, conn net.Conn) {
+	panic("not implemented") // TODO: Implement
 }
 
-func (hander *EchoHandler) Handler(ctx context.Context, conn net.Conn) {
-	if hander.closing.Get() {
-		conn.Close()
-	}
-	client := &EchoClient{
-		Conn: conn,
-	}
-	hander.activeConn.Store(client, struct{}{})
-	reader := bufio.NewReader(conn)
-	for {
-		msg, err := reader.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				logger.Info("connection close")
-				hander.activeConn.Delete(client)
-			} else {
-				logger.Warn(err)
-			}
-			return
-		}
-		client.Waiting.Add(1)
-		b := []byte(msg)
-		conn.Write(b)
-		client.Waiting.Done()
-	}
-}
-
-func (hander *EchoHandler) Close() error {
+func (handler *RespHandler) Close() error {
 	logger.Info("handler shutting down")
-	hander.closing.Set(true)
-	hander.activeConn.Range(func(key, value any) bool {
-		client := key.(*EchoClient)
+	handler.closing.Set(true)
+	handler.activeConn.Range(func(key, value any) bool {
+		client := key.(*connection.Connection)
 		client.Close()
 		return true
 	})
-
-	return nil
-}
-
-type EchoClient struct {
-	Conn    net.Conn
-	Waiting wait.Wait
-}
-
-func (e *EchoClient) Close() error {
-	e.Waiting.WaitWithTimeout(10 * time.Second)
-	e.Conn.Close()
+	handler.db.Close()
 	return nil
 }
